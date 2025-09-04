e29a2724ddd71ca83a5e2cd5fde3511d1db4ca59
Victor Chang <victor@squid.cloud>
2025-09-03 20:38:30 -0700

    Restrict modifying the LLM model.

--- backend/src/service/ai-security-service.ts
+++ backend/src/service/ai-security-service.ts
@@ -1,4 +1,4 @@
-import { secureAiAgent, SquidService } from '@squidcloud/backend';
+import { secureAiAgent, SecureAiAgentContext, SquidService } from '@squidcloud/backend';
 /*
   This service allows only authenticated users to chat with the AI assistant.
   For more information on securing your Squid AI assistant, see the documentation:
@@ -6,7 +6,13 @@ import { secureAiAgent, SquidService } from '@squidcloud/backend';
 */
 export class AiSecurityService extends SquidService {
   @secureAiAgent('squid-facts')
-  allowChat(): boolean {
+  allowChat(context: SecureAiAgentContext): boolean {
+    if (context.options?.model !== undefined) {
+      // Can enforce that the client is not requesting a different LLM model
+      // than the one the agent was configured to use.
+      return false;
+    }
+    // Enforce the user is logged in.
     return this.isAuthenticated();
   }
 }
